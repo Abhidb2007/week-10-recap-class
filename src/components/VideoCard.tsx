d9e7d43a3add3e3265d4218a1b9900138a1a350e
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-10 00:25:58 +0530

    add code

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,5 +1,15 @@
 export function VideoCard(){
     return<div>
         <img src="photo.jpg"></img>
+        <div className="gird grid-cols-12 bg-red">
+
+        </div>
+        <div className="grid-span-2">
+            <img src="/photo.jpg"></img>
+        </div>
+        <div className="grid-span-3">
+            kgf: chapter 2
+            yash
+        </div>
     </div>
 }
