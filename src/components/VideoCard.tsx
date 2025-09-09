c1d2f4ba1bf7020803db4c1fdcda5204de6914e0
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-10 02:53:58 +0530

    photo add

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,26 +1,30 @@
-export function VideoCard(){
+export function VideoCard(props:any){
     return <div>
-        <img src="/photo.jpg"></img>
+        <img src="/photo.jpg"
+        className="rounded-xl"></img>
         <div className=" grid grid-cols-12">
-            <div className="col-span-2">
-                <img className={"rounded-full w-16 h-16"} src="/photo.jpg"></img>
-            </div>
-            <div className="col-span-10
+         <div className="col-span-2">
+                <img className={"rounded-full w-15 h-15"} src={props.image}></img>
+         </div>
+          <div className="col-span-10
             pl-0
             font-cursors
             
             text-lg
             ">
-                kgf:chapter 2
-               <p> 1.3M views . 2 days ago</p>
+                <div>
+                {props.title}
+                </div>
 
-            </div>
-            <div className="col-span-1 pl-5">
-                abhi
+            
+            <div className="col-span-10 pl-5">
+                {props.author}
             </div>
             <div className="col-span-12 pl-25 text-gray-600 text-base">
-                action.drama||45min
-            </div>    
+                {props.views} | {props.timestamp}
+            </div>
+          </div> 
+              
         </div>
     </div>
 }
