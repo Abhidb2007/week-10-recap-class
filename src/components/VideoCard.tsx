d9a1e743c0dbf20a5f89b74252097bddd554b630
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-12 10:41:45 +0530

    add code

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -4,7 +4,7 @@ export function VideoCard(props:any){
         className="rounded-xl"></img>
         <div className=" grid grid-cols-12">
          <div className="col-span-2">
-                <img className={"rounded-full w-15 h-15"} src={props.image}></img>
+            <img className={"rounded-full w-15 h-15"} src="/thumb.jpg"></img>
          </div>
           <div className="col-span-10
             pl-0
@@ -17,10 +17,10 @@ export function VideoCard(props:any){
                 </div>
 
             
-            <div className="col-span-10 pl-6">
+            <div className="col-span-12 pl-1">
                 {props.author}
             </div>
-            <div className="col-span-12 pl-29 text-gray-600 text-base">
+            <div className="col-span-12 pl-1 text-gray-600 text-base">
                 {props.views} | {props.timestamp}
             </div>
           </div> 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
-import{Inter} from "next/font/google"
+import{Abhaya_Libre, Inter} from "next/font/google"
 import {VideoCard} from "../components/VideoCard"
 const inter=Inter({subsets:["latin"]})
 export default function Home(){
   return(
     <div>
-      youtube
-      <VideoCard></VideoCard>
+  
+      <VideoCard
+      title={"how to learn in coding|code with me"}
+      author={"Abhay"}
+      views={"12m"}
+      image={"photo.jpg"}
+      timestamp={"2days to go"}
+      ></VideoCard>
     </div>
   )
 }
